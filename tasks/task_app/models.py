ebe5182d5298a017ee977fe31a5cdd36c994eaf7
jyu <yjs01016@naver.com>
2023-01-28 18:13:57 +0900

    Task model 생성

--- tasks/task_app/models.py
+++ tasks/task_app/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 
 # Create your models here
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
