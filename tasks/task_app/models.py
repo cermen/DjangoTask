468da4de51c793575ea67e280b69ee0469620c5d
jyu <yjs01016@naver.com>
2023-02-01 16:56:00 +0900

    admin 추가

--- tasks/task_app/models.py
+++ tasks/task_app/models.py
@@ -6,3 +6,6 @@ class Task(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateField()
     end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
