0b85b47cc5c0dc88a2f30080d94a75d8176839a7
jyu <yjs01016@naver.com>
2023-02-02 03:39:34 +0900

    목록 및 조회 페이지 구현

--- tasks/task_app/views.py
+++ tasks/task_app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Task
 
 
 # Create your views here.
 def index(request):
-    print('request index - ')
-    return render(request, 'index.html')
+    task_list = Task.objects.order_by('id')
+    context = {'task_list': task_list}
+    return render(request, 'task_app/task_list.html', context)
+
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    context = {'task': task}
+    return render(request, 'task_app/task_detail.html', context)
