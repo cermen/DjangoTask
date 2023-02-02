e8b3264a387809f0b18356fc72116cb082f9a785
jyu <yjs01016@naver.com>
2023-02-02 18:04:56 +0900

    Memo 객체 추가

--- tasks/task_app/models.py
+++ tasks/task_app/models.py
@@ -3,9 +3,15 @@ from django.db import models
 
 # Create your models here
 class Task(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     start_date = models.DateField()
     end_date = models.DateField()
 
     def __str__(self):
-        return self.name
+        return self.title
+
+
+class Memo(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
