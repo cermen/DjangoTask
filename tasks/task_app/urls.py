ad94c34a38c8796e766bfadfb6c12cc1e6fa0196
jyu <yjs01016@naver.com>
2023-02-02 20:17:37 +0900

    메모 등록 구현

--- tasks/task_app/urls.py
+++ tasks/task_app/urls.py
@@ -8,4 +8,5 @@ app_name = 'task_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:task_id>/', views.detail, name='detail'),
+    path('memo/create/<int:task_id>', views.memo_create, name='memo_create')
 ]
--- tasks/task_app/views.py
+++ tasks/task_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Task
 
 
@@ -13,3 +14,9 @@ def detail(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     context = {'task': task}
     return render(request, 'task_app/task_detail.html', context)
+
+
+def memo_create(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.memo_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('task_app:detail', task_id=task.id)
