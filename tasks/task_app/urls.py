79ce688b0d63ec815ccf53d348c8225126ecf0d6
jyu <yjs01016@naver.com>
2023-02-02 16:36:54 +0900

    URL 별칭 지정

--- tasks/task_app/urls.py
+++ tasks/task_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'task_app'
+
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:task_id>', views.detail),
+    path('', views.index, name='index'),
+    path('<int:task_id>/', views.detail, name='detail'),
 ]
