bfe4c66bf19bea1af30a0bd09e8a18ed9f83b610
jyu <yjs01016@naver.com>
2023-02-03 23:50:05 +0900

    admin에 Memo 객체 추가

--- tasks/task_app/admin.py
+++ tasks/task_app/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Memo
 
 # Register your models here.
 
 
 class TaskAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+    search_fields = ['title']
+
+
+class MemoAdmin(admin.ModelAdmin):
+    search_fields = ['content']
 
 
 admin.site.register(Task, TaskAdmin)
+admin.site.register(Memo, MemoAdmin)
